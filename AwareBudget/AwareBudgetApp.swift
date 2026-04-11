80ed0187f1f07056275c5d6f7b67b2c033b0bffc
Arabell Haythorpe <ahaythorpe2025@gmail.com>
2026-04-12 06:50:04 +1000

    Initial Commit

---
+++ AwareBudget/AwareBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwareBudgetApp.swift
+//  AwareBudget
+//
+//  Created by Arabella Haythorpe on 12/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct AwareBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
